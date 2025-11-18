3bf11ed9649f9fa3d4bcd97f0552439ed3b8c3ee
roomroofroot <vittal.kensri@gmail.com>
2025-11-18 11:40:31 +0530

    Add main.py, some fixes.

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from game_logic import *
+from board import *
+
+print()
+
+if sum(count(readsave()[1:10])) == 64:
+    resetsave()
+else:
+	s = input("Retrieve previous game(y/n)? ")
+	if s.lower() == 'n':
+		resetsave()
+
+state = readsave()[1:10]
+colour = readsave()[0]
+
+code = 1
+while code:
+    print()
+    print(colour, "to play")
+    print(*state, sep="\n")
+    print()
+    m = listmoves(state, colour)
+    if len(m) == 0:
+        print("No moves left, switching player")
+        colour = 3 - colour
+        if len(listmoves(state, colour)) == 0:
+            print("No moves left")
+            code = 0
+            break
+    else:
+        print("Valid moves:", *sorted(list(m)))
+        x, y = map(int, input("Enter valid x, y values: ").split())
+        if (x, y) in m:
+            state = moves(state, colour, x, y)
+            writesave(state, colour)
+            colour = 3 - colour
