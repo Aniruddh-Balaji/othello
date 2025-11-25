ebe88e5779b60fa8ee12a03aa8fc9e5ea3a77190
roomroofroot <vittal.kensri@gmail.com>
2025-11-25 20:00:50 +0530

    cli.py revamp

--- src/cli.py
+++ src/cli.py
@@ -1,14 +1,17 @@
 from game_logic import *
 from board import *
 
-print()
+print("\nWELCOME TO OTHELLO\n")
 
 if sum(count(readsave()[1:10])) == 64:
+    print("\nPrevious game ended, starting new game\n")
     resetsave()
 else:
-    s = input("Retrieve previous game(y/n)? ")
-    if s.lower() == "n":
+    s = input("[1] Start new game\n[2] Load previous game\n[3] Quit\nOption: ")
+    if s == '1':
         resetsave()
+    elif s != '2':
+        exit()
 
 state = readsave()[1:10]
 colour = readsave()[0]
@@ -16,12 +19,13 @@ colour = readsave()[0]
 code = 1
 while code:
     print()
-    print(colour, "to play")
-    print(*state, sep="\n")
+    print(colour, "to play\n")
+    for i in state:
+        print(*i)
     print()
     m = listmoves(state, colour)
     if len(m) == 0:
-        print("No moves left, switching player")
+        print("\nNo moves left, switching player")
         colour = 3 - colour
         if len(listmoves(state, colour)) == 0:
             print("No moves left")
@@ -29,8 +33,30 @@ while code:
             break
     else:
         print("Valid moves:", *sorted(list(m)))
-        x, y = map(int, input("Enter valid x, y values: ").split())
-        if (x, y) in m:
-            state = moves(state, colour, x, y)
-            colour = 3 - colour
-            writesave(state, colour)
+        s = input("Your move\nx y: ").split()
+        try:
+            x, y = map(int, s)
+            if (x, y) in m:
+                state = moves(state, colour, x, y)
+                colour = 3 - colour
+                writesave(state, colour)
+            else:
+                print("\nInvalid move, try again")
+
+        except:
+            print("\nInvalid input")
+            op = input("[1] try again\n[2] quit\nOption: ")
+            if op == '2':
+                break
+
+print("\nFinal Scores\nPlayer 1:", count(state)[0], "\nPlayer 2:", count(state)[1])
+
+if code == 0:
+    if count(state)[0] > count(state)[1]:
+        print("\nPlayer 1 wins!")
+    elif count(state)[1] > count(state)[0]:
+        print("\nPlayer 2 wins!")
+    else:
+        print("\nDraw")
+
+print("\nOk friend, bye")
